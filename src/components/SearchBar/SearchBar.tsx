1e44cb22db0b8cb2da20fba6ac4fc137907d71eb
Vitaliy <vitalikkalamunak@gmail.com>
2025-08-02 18:06:22 +0300

    03-react-movies

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,51 @@
+import type { FormEvent } from "react";
+import toast from "react-hot-toast";
+import styles from "./SearchBar.module.css";
+
+interface Props {
+  onSubmit: (query: string) => void;
+}
+
+export function SearchBar({ onSubmit }: Props) {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const query = form.elements.namedItem("query") as HTMLInputElement;
+
+    if (!query.value.trim()) {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(query.value.trim());
+    form.reset();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a
+          className={styles.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            className={styles.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={styles.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
